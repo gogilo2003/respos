93dd3bdaad4ad3b149186a6be8fda5b7a04eef04
Ogilo George <gogilo2003@hotmail.com>
2026-08-07 13:14:03 +0300

    Additional users in the UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Role;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -15,14 +14,59 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $adminRole = Role::where('name', 'admin')->first();
+        $users = [
+            [
+                'name' => 'System Administrator',
+                'username' => 'admin',
+                'email' => 'admin@respos.com',
+                'role' => 'admin',
+            ],
+            [
+                'name' => 'Restaurant Manager',
+                'username' => 'manager',
+                'email' => 'manager@respos.com',
+                'role' => 'manager',
+            ],
+            [
+                'name' => 'Head Cashier',
+                'username' => 'cashier',
+                'email' => 'cashier@respos.com',
+                'role' => 'cashier',
+            ],
+            [
+                'name' => 'Floor Waiter',
+                'username' => 'waiter',
+                'email' => 'waiter@respos.com',
+                'role' => 'waiter',
+            ],
+            [
+                'name' => 'Chef Kitchen',
+                'username' => 'kitchen',
+                'email' => 'kitchen@respos.com',
+                'role' => 'kitchen',
+            ],
+            [
+                'name' => 'Test Customer',
+                'username' => 'customer',
+                'email' => 'customer@respos.com',
+                'role' => 'customer',
+            ],
+        ];
 
-        User::factory()->create([
-            'name' => 'System Administrator',
-            'email' => 'admin@respos.com',
-            'password' => Hash::make('password'),
-            'role_id' => $adminRole->id,
-            'is_active' => true,
-        ]);
+        foreach ($users as $userData) {
+            $role = Role::firstOrCreate(['name' => $userData['role']]);
+
+            User::updateOrCreate(
+                ['email' => $userData['email']],
+                [
+                    'name' => $userData['name'],
+                    'username' => $userData['username'],
+                    'password' => Hash::make('password'),
+                    'role_id' => $role->id,
+                    'is_active' => true,
+                    'email_verified_at' => now(),
+                ]
+            );
+        }
     }
 }
